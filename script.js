e0ae974650b77fb49add3d572f7b15b2caab7f8a
Chelsea <186109435+seaarmadillo@users.noreply.github.com>
2025-02-17 15:04:42 -0600

    add value to buttons add buttons to button div

--- script.js
+++ script.js
@@ -8,3 +8,14 @@ const buttonValues = [
 
 const rightSymbols = ["÷", "x", "-", "+"];
 const topSymbols = ["AC", "+/-", "%"];
+
+for(let i=0; i < buttonValues.length; i++) {
+    //<button></button>
+    let value = buttonValues[i];
+    let button = document.createElement("button");
+    button.innerText = value;
+}
+
+//add buttons to div
+
+document.getElementById("buttons").appendChild(button);
