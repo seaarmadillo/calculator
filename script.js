5d908da0a903f786bfb3e57906a92b69cbab3eaf
Chelsea <186109435+seaarmadillo@users.noreply.github.com>
2025-02-17 15:39:44 -0600

    add button styling to top and right button styles set buttons in grid set background color

--- script.js
+++ script.js
@@ -6,16 +6,28 @@ const buttonValues = [
     "0", ".", "=" 
 ];
 
-const rightSymbols = ["÷", "x", "-", "+"];
+const rightSymbols = ["÷", "x", "-", "+", "="];
 const topSymbols = ["AC", "+/-", "%"];
 
-for(let i=0; i < buttonValues.length; i++) {
+for (let i = 0; i < buttonValues.length; i++) {
     //<button></button>
     let value = buttonValues[i];
     let button = document.createElement("button");
     button.innerText = value;
-}
+
+    //styling button colors
+
+    if(rightSymbols.includes(value)){
+        button.style.backgroundColor = "#D1AC00";
+    }
+
+    else if (topSymbols.includes(value)){
+        button.style.backgroundColor = "#FAF4D3"
+        button.style.color = "#0C1618"
+    }
 
 //add buttons to div
+    document.getElementById("buttons").appendChild(button);
+}
 
-document.getElementById("buttons").appendChild(button);
+
